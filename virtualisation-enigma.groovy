bebd9e71ee426368f48ee945697787c36418d4b9
Maxime Josien <maxime@disruptual.com>
2019-03-21 12:30:55 +0100

    Script groovy + modification docker

---
+++ virtualisation-enigma.groovy
@@ -0,0 +1,14 @@
+job('job-v1') {
+    scm {
+        git('https://github.com/maximejosien/VirtualisationEnigma', "v1") { node ->
+            node / gitConfigName('Maxime')
+            node / gitConfigEmail('maxime.josien@estiam.com')
+        }
+    }
+    triggers {
+        scm('H/60 * * * *')
+    }
+    steps {
+        shell("ls")
+    }
+}
