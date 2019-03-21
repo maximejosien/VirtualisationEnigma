7bcf4569f13d79b7cb6915e65b92cae7f488b4b8
Maxime Josien <maxime@disruptual.com>
2019-03-21 17:12:56 +0100

    Commit start.sh

---
+++ start.sh
@@ -0,0 +1,13 @@
+git clone git@github.com:Akasam/samplephpwebsite.git
+
+cd ./samplephpwebsite
+git checkout v3
+cd ../
+
+docker-compose up -d --build php nginx
+
+echo "Terminer Site V3"
+read ""
+
+docker-compose down
+rm -rf samplephpwebsite
