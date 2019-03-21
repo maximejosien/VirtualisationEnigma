c983369210f2cb17b6c26ca2a3707703de0a8880
Maxime Josien <maxime@disruptual.com>
2019-03-21 15:18:57 +0100

    Projet v3

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
-git clone https://github.com/Akasam/samplephpwebsite.git
+git clone git@github.com:Akasam/samplephpwebsite.git
+
+cd ./samplephpwebsite
+
+git checkout v3
+
+cd ..
 
 docker-compose up -d --build
--- virtualisationenigma.groovy
+++ virtualisationenigma.groovy
@@ -1,6 +1,6 @@
 job('job-v1') {
     scm {
-        git('https://github.com/maximejosien/VirtualisationEnigma', "v1") { node ->
+        git('https://github.com/maximejosien/VirtualisationEnigma', "v3") { node ->
             node / gitConfigName('Maxime')
             node / gitConfigEmail('maxime.josien@estiam.com')
         }
